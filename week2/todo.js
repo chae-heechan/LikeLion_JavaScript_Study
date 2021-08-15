84f8e268fbe22439676a1cb303da3b470859c9e7
Chae hee chan <eqholic1125@gmail.com>
2021-08-15 23:06:39 +0900

    feat: 완료시 취소선 기능 구현

--- week2/todo.js
+++ week2/todo.js
@@ -1,6 +1,16 @@
+// 체크박스 이미지
+const img = [
+  {
+    file : 'checkBox.png'
+  },
+  {
+    file : 'checkBoxActive.png'
+  }
+]
+
 const inputBox = document.querySelector("#input-box");
 const list = document.querySelector("#list");
-const checkBox = document.querySelector(".check-box");
+let checkBox = document.querySelector(".check-box");
 
 // 엔터 검사 메서드
 const isEnter = () =>{
@@ -13,22 +23,38 @@ const isEnter = () =>{
 const enterInput = () =>{
   const itemBox = document.createElement('div');
   const item = document.createElement("p");
-  const checkBox = document.createElement("button");
+  const checkBox = document.createElement("img");
 
   itemBox.classList.add("item-box");
   item.classList.add("item");
   item.innerHTML = inputBox.value;
   checkBox.classList.add("check-box");
+  checkBox.src = img[0].file;
+  checkBox.setAttribute('onclick', "clickCheckBox()"); // <img onclick = click()>
+  console.log(checkBox);
 
   itemBox.appendChild(checkBox);
   itemBox.appendChild(item);
   list.appendChild(itemBox);
   // input-box 내용 지우기
+  inputBox.value = "";
+
+  // checkBox = document.querySelector(".check-box");
 }
 
 function clickCheckBox(){
-  // 클릭시 item에 취소선 클래스 추가
-  // 이 부분 다시 설계 해야함
+  const parentDiv = event.target.parentNode;
+
+  // 기본인 경우 => 엑티브로
+  if (!parentDiv.childNodes[0].src.includes("Active")) {
+    parentDiv.childNodes[0].src = `${img[1].file}`;
+    parentDiv.childNodes[1].classList.add("cancellation-line");
+    // 액티브인 경우 => 기본으로
+  } else {
+    parentDiv.childNodes[0].src = `${img[0].file}`;
+    parentDiv.childNodes[1].classList.remove("cancellation-line");
+
+  }
 }
 
-checkBox.addEventListener("click", clickCheckBox);
+// checkBox.addEventListener("click", clickCheckBox);
